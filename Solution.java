08a245cbea6dc7204b57f27a7c56b5535a7612fb
Abhishek Kumar <ytabhishek2002@gmail.com>
2025-07-28 12:51:30 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        java.util.HashMap<Integer, Integer> map = new java.util.HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = sol.twoSum(nums, target);
+
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
